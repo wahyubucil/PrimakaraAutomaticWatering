412399c189a85d696a1bc9ccc481f59376ee3564
Wahyu Budi Saputra <wahyubucil@gmail.com>
2018-06-27 12:47:06 +0800

    Create button for open wifi settings

--- app/src/main/java/dev/primakara/automatic_watering/primakaraautomaticwatering/MainActivity.java
+++ app/src/main/java/dev/primakara/automatic_watering/primakaraautomaticwatering/MainActivity.java
@@ -1,10 +1,14 @@
 package dev.primakara.automatic_watering.primakaraautomaticwatering;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import butterknife.BindView;
@@ -12,7 +16,8 @@ import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
-    @BindView(R.id.tv_test) TextView testTextView;
+    @BindView(R.id.tv_test) TextView mTestTextView;
+    @BindView(R.id.btn_connect) Button mConnectButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +27,14 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         checkWifiConnected();
+
+        mConnectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Settings.ACTION_WIFI_SETTINGS);
+                startActivity(intent);
+            }
+        });
     }
 
     private void checkWifiConnected() {
@@ -40,9 +53,9 @@ public class MainActivity extends AppCompatActivity {
 
         if (ssid != null) {
             if (ssid.equals(getString(R.string.wifi_ssid))) {
-                testTextView.setText("Wifi connected");
+                mTestTextView.setText("Wifi connected");
             } else {
-                testTextView.setText("Failed to connect");
+                mTestTextView.setText("Failed to connect");
             }
         }
     }
