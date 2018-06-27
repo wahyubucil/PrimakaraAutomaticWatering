589dacc81efd083004996cd5fbc997184ebcd244
Wahyu Budi Saputra <wahyubucil@gmail.com>
2018-06-27 12:22:07 +0800

    Create test for wifi detect

--- app/src/main/java/dev/primakara/automatic_watering/primakaraautomaticwatering/MainActivity.java
+++ app/src/main/java/dev/primakara/automatic_watering/primakaraautomaticwatering/MainActivity.java
@@ -1,13 +1,49 @@
 package dev.primakara.automatic_watering.primakaraautomaticwatering;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.tv_test) TextView testTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        checkWifiConnected();
+    }
+
+    private void checkWifiConnected() {
+        WifiManager wifiManager = (WifiManager) getApplicationContext()
+                .getSystemService(Context.WIFI_SERVICE);
+
+        WifiInfo wifiInfo = null;
+        if (wifiManager != null) {
+            wifiInfo = wifiManager.getConnectionInfo();
+        }
+
+        String ssid = null;
+        if (wifiInfo != null) {
+            ssid = wifiInfo.getSSID();
+        }
+
+        if (ssid != null) {
+            if (ssid.equals(getString(R.string.wifi_ssid))) {
+                testTextView.setText("Wifi connected");
+            } else {
+                testTextView.setText("Failed to connect");
+            }
+        }
     }
 }
